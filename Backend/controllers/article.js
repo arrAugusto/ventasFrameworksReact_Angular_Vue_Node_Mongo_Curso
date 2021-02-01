ace80ed04976f1843af424aac8eb331809440516
agr12 <agr12@DESKTOP-2MO9KA5>
2021-01-31 22:13:19 -0600

    Creando api rest para guardar json en mongodb y consumiendo construyendo api

--- Backend/controllers/article.js
+++ Backend/controllers/article.js
@@ -1,5 +1,9 @@
 'use stric'
 
+var validator = require('validator');
+
+var Article = require('../modelo/article');
+
 
 var controller = {
   datosCurso: (req, res)=>{
@@ -13,8 +17,70 @@ var controller = {
         return res.status(200).send({
             message: 'Soy la acción test del controlador de articulos'
         });
-    }
+    },
     
+    save: (req, res)=>{
+        //obtener datos post que el usuario envie
+        var params = req.body;
+        console.log(params); 
+        //validar datos con la libreria validator
+        try{
+            var validateTitle = !validator.isEmpty(params.title); 
+            var validatecontent = !validator.isEmpty(params.content); 
+        }catch(err){
+            return res.status(200).send({
+                status: 'error',
+                message: 'datos invalidos'
+            });
+    
+        }
+
+        // si los validator son correctos
+        if (validateTitle && validatecontent){
+        //crear objeto a guardar
+            var article = new Article();
+        //asignando valores
+            article.title = params.title;
+            article.content = params.content;
+            article.image = null;
+
+            
+            
+        //guardar articulos
+            article.save((err, articleStore)=>{
+                //articulos no se guardaron correctamente
+                if (err || !articleStore){
+                    return res.status(200).send({
+                        status: 'error',
+                        message: 'datos invalidos'
+                    });
+        
+                }
+                //sino se guardan los articulos
+                return res.status(200).send({
+                    status: 'success',
+                    article: articleStore
+                });
+                    
+            })
+        //responder respuesta
+            return res.status(200).send({
+                status: 'success',
+                article
+            });
+
+        }else{
+            return res.status(200).send({
+                status: 'error',
+                message: 'datos invalidos'
+            });
+    
+        }
+
+        return res.status(200).send({
+            article: params
+        });
+    }
 };//end controler
 
 module.exports = controller;
--- Backend/routes/article.js
+++ Backend/routes/article.js
@@ -6,8 +6,12 @@ var ArticleController = require('../controllers/article');
 
 var router = express.Router();
 
+//rutas de prueba
 router.get('/datosCurso', ArticleController.datosCurso);
 router.get('/test-de-controller', ArticleController.test);
+
+//rutas utiles
+router.post('/save', ArticleController.save);
 
 
 module.exports = router;
